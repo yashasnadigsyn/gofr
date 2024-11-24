0ef45d48225fdf695bc98cb83374b6b989b0ad72
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2024-11-24 06:24:13 +0530

    Update response.go

--- pkg/gofr/http/response/response.go
+++ pkg/gofr/http/response/response.go
@@ -1,13 +1,3 @@
-package response
-
-import (
-	"net/http"
-)
-
-type Response struct {
-	Data    any               `json:"data"`
-	Headers map[string]string `json:"-"`
-}
 
 func (resp Response) SetCustomHeaders(w http.ResponseWriter) {
 	for key, value := range resp.Headers {
